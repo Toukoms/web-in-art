4c106ff6f8a3b2c4a7978ad64033ae790b434ff2
Toukom's <tokiniaina090605@gmail.com>
2024-05-28 15:50:24 +0300

    feat: create footer component

--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="wrapper flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4 p-5 text-center">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="WebInArt logo"
+          />
+        </Link>
+        <p>©2024 WebInArt. All right Reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
