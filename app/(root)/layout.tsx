0a141dd9018ee9ff97b40efbfa1f39ba85716d8d
Toukom's <tokiniaina090605@gmail.com>
2024-05-28 15:48:57 +0300

    feat: create nav links

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen justify-between">
       <Header />
       <main>{children}</main>
       <Footer />
