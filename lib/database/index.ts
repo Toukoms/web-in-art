70f3b383c9c67e543b0bc2da1bc783e6417edbb9
Toukom's <tokiniaina090605@gmail.com>
2024-07-30 18:40:20 +0300

    fix: createUser action to create user in mongodb.

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null }
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
@@ -12,9 +12,9 @@ export const connectToDatabase = async () => {
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     dbName: "webinart",
     bufferCommands: false,
-  }) 
+  });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
